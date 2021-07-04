77ad5faf802d125dad0620a686d6fdfd5bdf83e1
PetteriM1 <petteri.malkavaara1@gmail.com>
2021-07-04 18:46:02 +0300

    Initial commit

---
+++ src/main/java/me.petterim1.ns/NoSpaceInName.java
@@ -0,0 +1,25 @@
+package me.petterim1.ns;
+
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.server.DataPacketReceiveEvent;
+import cn.nukkit.network.protocol.LoginPacket;
+import cn.nukkit.plugin.PluginBase;
+
+public class NoSpaceInName extends PluginBase implements Listener {
+
+    private String replaceWith;
+
+    public void onEnable() {
+        saveDefaultConfig();
+        replaceWith = getConfig().getString("replaceWith");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void DataPacketReceiveEvent(DataPacketReceiveEvent e) {
+        if (e.getPacket() instanceof LoginPacket) {
+            ((LoginPacket) e.getPacket()).username = ((LoginPacket) e.getPacket()).username.replace(" ", replaceWith);
+        }
+    }
+}
